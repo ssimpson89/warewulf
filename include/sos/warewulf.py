a66c90d5788aacf9021287bff458e6b26b3441c7
Stephen Simpson <ssimpson89@users.noreply.github.com>
2025-02-20 16:16:37 -0600

    Add sosreport plugin for Warewulf with RPM build support
    Signed-off-by: Stephen Simpson <ssimpson89@users.noreply.github.com>

---
+++ include/sos/warewulf.py
@@ -0,0 +1,35 @@
+from sos.report.plugins import Plugin, IndependentPlugin
+
+class Warewulf(Plugin, IndependentPlugin):
+
+    short_desc = 'Warewulf provisioning platform'
+    
+    plugin_name = 'warewulf'
+    services = ('warewulfd')
+    packages = ('warewulf')
+
+    def setup(self):
+       
+        self.add_copy_spec([
+          "/var/lib/warewulf/overlays/",
+          "/usr/share/warewulf/overlays/",
+          "/var/log/warewulfd.log",
+          "/etc/warewulf/"
+        ])
+        
+        self.add_forbidden_path([
+          "/var/lib/warewulf/overlays/wwinit/rootfs/warewulf/wwclient",
+          "/usr/share/warewulf/overlays/wwinit/rootfs/warewulf/wwclient"
+        ])
+
+        self.add_cmd_output([
+            "wwctl node list",
+            "wwctl node list -a",
+            "wwctl container list",
+            "wwctl profile list",
+            "wwctl profile list -a",
+            "wwctl image kernels",
+            "wwctl version"
+        ])
+        
+        self.add_journal(units="warewulfd.service")
